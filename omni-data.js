6b1edfac96d198d752edef7e6038f0f034a6735d
Emily Wilson <emily.wilson@teamspace.ca>
2014-06-05 08:56:33 -0300

    Fixed typo

--- omni-data.js
+++ omni-data.js
@@ -4,6 +4,6 @@
 		"linkName" : "dev/bento/link",
 		"prop1"    : "value of prop 1",
 		"eVar1"    : "value of evar 1",
-		"omnitureContextData" : {"gameName:"nameOfGame", "btg.appType":"testing", "v.nick.gameName":"newValue"}
+		"omnitureContextData" : {"contextKey":"contextValue", "btg.appType":"testing", "v.nick.gameName":"newValue"}
 	}
 ]
