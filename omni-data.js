45acf0089bc75ba067f6bb9885683483aaadb164
Emily Wilson <emily.wilson@teamspace.ca>
2014-06-06 13:52:03 -0300

    testing

--- omni-data.js
+++ omni-data.js
@@ -4,6 +4,5 @@
 		"linkName" : "dev/bento/link",
 		"prop1"    : "value of prop 1",
 		"eVar1"    : "value of evar 1",
-		"omnitureContextData" : {"v.dummyContext1":"value1","contextKey":"contextValue", "btg.appType":"testing", "v.nick.gameName":"newValue"}
 	}
 ]
