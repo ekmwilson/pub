49a9f5b532ea92d071f2ce2e1834941f6e4c5a5d
Emily Wilson <emily.wilson@teamspace.ca>
2014-06-11 09:20:48 -0300

    testing bumper

--- doubleclick_data_300x50_no_dfp_mraid.js
+++ doubleclick_data_300x50_no_dfp_mraid.js
@@ -4,7 +4,7 @@
 "doubleclickAdSize":"300x50",
 "doubleclickIsDfp":false,
 "doubleclickAdIndicatorVisible":false,
-"doubleclickAddBumper":true,
+"doubleclickAddBumper":false,
 "doubleclickDfpData":
 {
     "metadata": {
