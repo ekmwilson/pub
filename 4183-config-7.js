47c5cc5eb18c24eb076903d4b894e4d67a3c42e8
Emily Wilson <emily.wilson@teamspace.ca>
2014-07-21 16:30:44 -0300

    preparing for 4183 demo

---
+++ 4183-config-7.js
@@ -0,0 +1,16 @@
+[
+	{
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "test",
+		"omnitureChannel" : "Bento TestRig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "test1234",
+		"omnitureDebugLogging" : "true",
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true"
+	}
+]
---
+++ 4183-data-1.js
@@ -0,0 +1,14 @@
+[
+	{
+		"pageName" : "test/bento/testrig",
+		"linkName" : "test/bento/link",
+		"prop1"    : "value of prop  1 Data file",
+		"prop15"   : "value of prop 15 data file",
+		"prop115"  : "value of prop 115 data file",
+		"prop36"  : "value of prop 36 aka the show information data file",
+		"eVar1"    : "value of var 1 data file",
+		"eVar16"   : "value of var 16 data file",
+		"eVar116"  : "value of var 116 data file",
+		"omnitureContextData" : {"v.dummyContext11":"11","v.dummyContext22":"22", "dummyContextVATest":"123","nick.gamedata": "val1"}
+	}
+];
