0161bfc7d5d3f23debf4f34828b1bf849c2cd5e9
Emily Wilson <emily.wilson@teamspace.ca>
2015-01-13 15:46:45 -0400

    New temp file for testing

---
+++ temp.js
@@ -0,0 +1,9 @@
+[
+	{
+		"pageName" : "dev/bento/testrig",
+		"linkName" : "dev/bento/link",
+		"prop1"    : "value of prop 1",
+		"eVar1"    : "value of evar 1",
+		"contextData" : { "key1":"val2","key2":"val3"}
+	}
+]
