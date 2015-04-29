140f2c3ac254ccacaa7ee6fc263f85849aef3253
Emily Wilson <emily.wilson@atwoodgroup.ca>
2015-04-29 15:55:10 -0300

    Update omni-data-1.js

--- omni-data-1.js
+++ omni-data-1.js
@@ -6,6 +6,6 @@
 		"eVar10"    : "value of evar 10",
 		"eVar9"    : "value of evar 9",
 		"eVar39"    : "value of evar 39",
- 		"contextData" : {"eName":"value1","btg.appType":"testing", "v.nick.gameName":"newValue"}
+ 		"contextData" : {"contextDataKey1trackpage" : "testcontextvalue1trackpage", "contextDataKey2trackpage" : "testcontextvalue2trackpage", "m.testcontext1" : "testcontext1", "nick.game":"nickgamecontext"}
 	}
 ]
