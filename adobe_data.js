5eafff10117e7d99d1fee3160de86635ada1e7dc
Emily Wilson <emily.wilson@atwoodgroup.ca>
2015-10-07 14:51:11 -0300

    Update adobe_data.js

--- adobe_data.js
+++ adobe_data.js
@@ -2,7 +2,7 @@
 	{
 		"pageName" : "Testrig Adobe Page",
 		"linkName" : "Testrig Adobe Link",
-		"trackingData" : {"xrs" : "12345"}
+		"trackingData" : {"xrs" : "12345"},
 		"v.appName" : "BTG Test App",
 		"v.appType" : "Android App",
 		"v.appVersion" : "1.0"
