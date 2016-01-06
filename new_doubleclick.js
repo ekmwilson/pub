3c1bf806e68410ff0129f0eb2e80dd0ca1f125fd
Emily Wilson <emily.wilson@teamspace.ca>
2016-01-06 10:08:32 -0400

    New config

---
+++ new_doubleclick.js
@@ -0,0 +1,24 @@
+[
+	{	
+		"doubleclickKeyValues":"testmode=celtra_mobile_300x250",
+		"doubleclickAdSize":"300x250",
+		"doubleclickDfpData": {
+		    "metadata": {
+		        "property": "nick_app",
+		        "hub": "home",
+		        "is_main": true
+		    },
+		    "mappings": {
+		        "ad-unit": {
+		            "1": "@property"
+		    	},
+		    	"key-values": {
+		        	"sea": "@season",
+		        	"ep": "@episode",
+		        	"foo": "bar"
+		    	}
+	    	    }	
+
+		}	
+	}
+]
