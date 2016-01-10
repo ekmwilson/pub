bd071ab0a05fccc4304a562fa825e163afbc4d88
Emily Wilson <emily.wilson@teamspace.ca>
2016-01-10 14:48:43 -0400

    Added data for private app use.

---
+++ bodypump/releaseTracklist.js
@@ -0,0 +1,82 @@
+[
+	{
+		"Release 96": {
+			"Warmup": {
+				"Artist": "Deorro x Chris Brown",
+				"Title":"Five More Hours"
+			},
+			"Squats": {
+				"Artist": "Tiesto and MOTI",
+				"Title":"Blow Your Mind"
+			},
+			"Chest": {
+				"Artist": "Along Pointless",
+				"Title":"Immortals"
+			},
+			"Back": {
+				"Artist": "Nero",
+				"Title":"The Thrill"
+			},
+			"Triceps": {
+				"Artist": "Panic! At the Disco",
+				"Title":"Hallelujah"
+			},
+			"Biceps": {
+				"Artist": "Flo Rida",
+				"Title":"Wobble"
+			},
+			"Lunges": {
+				"Artist": "Galantis",
+				"Title":"Peanut Butter Jelly"
+			},
+			"Shoulders": {
+				"Artist": "Zedd feat. Bahari",
+				"Title":"Addicted to a Memory"
+			},
+			"Core": {
+				"Artist": "Jason Derulo",
+				"Title":"Want you to want me"
+			}
+		},
+		"Release 95": {
+			"Warmup": {
+				"Artist": "Calvin Harris feat. Ellie Goulding",
+				"Title":"Outisde"
+			},
+			"Squats": {
+				"Artist": "Autoerotique & 4B",
+				"Title":"Bukem"
+			},
+			"Chest": {
+				"Artist": "Mark Ronson feat. Bruno Mars",
+				"Title":"Uptown Funk"
+			},
+			"Back": {
+				"Artist": "Blasterjaxx & DBSTF feat. Ryder",
+				"Title":"Beautiful World"
+			},
+			"Triceps": {
+				"Artist": "Fall Out Boy",
+				"Title":"Irresistible"
+			},
+			"Biceps": {
+				"Artist": "Flo Rida feat. Sage The Gemini & Lookas",
+				"Title":"GDFR"
+			},
+			"Lunges": {
+				"Artist": "Calvin Harris & R3Hab",
+				"Title":"Burnin"
+			},
+			"Shoulders": {
+				"Artist": "Three Days Grace",
+				"Title":"I Am Machine"
+			},
+			"Core": {
+				"Artist": "Maroon 5",
+				"Title":"Sugar"
+			}
+		}
+	}
+]
+	
+
