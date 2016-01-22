bed113ed8955b39e234979ab3d303383c0b25bc2
Emily Wilson <emily.wilson@teamspace.ca>
2016-01-22 10:57:53 -0400

    Test

--- bodypump/releaseTracklist.js
+++ bodypump/releaseTracklist.js
@@ -75,6 +75,120 @@
 				"Artist": "Maroon 5",
 				"Title":"Sugar"
 			}
+		},
+		"Release 94": {
+			"Warmup": {
+				"Artist": "Alesso feat. Tove Lo",
+				"Title":"Heroes (We Could Be)"
+			},
+			"Squats": {
+				"Artist": "Vice",
+				"Title":"Everybody Go!"
+			},
+			"Chest": {
+				"Artist": "Jessie J feat. 2 Chainz",
+				"Title":"Burnin' Up"
+			},
+			"Back": {
+				"Artist": "Scooter & Vassy",
+				"Title":"Today (Scooter Remix)"
+			},
+			"Triceps": {
+				"Artist": "Meghan Trainor",
+				"Title":"Lips Are Movin"
+			},
+			"Biceps": {
+				"Artist": "Nickelback",
+				"Title":"Get 'Em Up"
+			},
+			"Lunges": {
+				"Artist": "Dillon Francis feat. Major Lazer & Stylo G",
+				"Title":"We Make It Bounce"
+			},
+			"Shoulders": {
+				"Artist": "Zylent feat. Katatonic",
+				"Title":"This Is How It Goes"
+			},
+			"Core": {
+				"Artist": "Gwen Stefani",
+				"Title":"Baby Don't Lie"
+			}
+		},
+		"Release 93": {
+			"Warmup": {
+				"Artist": "Ariana Grande feat. Zedd",
+				"Title":"Break Free"
+			},
+			"Squats": {
+				"Artist": "Showtek feat. We are Loud & Sonny Wilson",
+				"Title":"Booyah (Lucky Date Remix)"
+			},
+			"Chest": {
+				"Artist": "Thirty Seconds to Mars",
+				"Title":"Kings and Queens"
+			},
+			"Back": {
+				"Artist": "Dash Berlin & Jay Cosmic feat. Colin McLoughlin",
+				"Title":"Here Tonight (Carita La Nina Radio Edit)"
+			},
+			"Triceps": {
+				"Artist": "Jessie J, Ariana Grande, & Nicki Minaj",
+				"Title":"Bang Bang"
+			},
+			"Biceps": {
+				"Artist": "Fall Out Boy",
+				"Title":"Centuries"
+			},
+			"Lunges": {
+				"Artist": "Klayplex feat. Virian",
+				"Title":"1941"
+			},
+			"Shoulders": {
+				"Artist": "Fedde le Grand",
+				"Title":"Twisted (Eptic Remix)"
+			},
+			"Core": {
+				"Artist": "Meghan Trainor",
+				"Title":"All About That Base"
+			}
+		},
+		"Release 92": {
+			"Warmup": {
+				"Artist": "All We Need Is Love",
+				"Title":"Ricki-Lee"
+			},
+			"Squats": {
+				"Artist": "Autoerotique vs Marc Noir",
+				"Title":"GO!"
+			},
+			"Chest": {
+				"Artist": "Fall Out Boy",
+				"Title":"The Pheonix"
+			},
+			"Back": {
+				"Artist": "BT Fractal & JES",
+				"Title":"Letting Go (LTN Radio Edit)"
+			},
+			"Triceps": {
+				"Artist": "Ariana Grande feat. Iggy Azalea",
+				"Title":"Problem"
+			},
+			"Biceps": {
+				"Artist": "KONGOS",
+				"Title":"Come With Me Now"
+			},
+			"Lunges": {
+				"Artist": "Guiltless Happy",
+				"Title":"NRG"
+			},
+			"Shoulders": {
+				"Artist": "Armin Van Buuren",
+				"Title":"Ping Pong (Radio Edit)"
+			},
+			"Core": {
+				"Artist": "Iggy Azalea feat. Charli XCX",
+				"Title":"Fancy"
+			}
 		}
 	}
 ]
