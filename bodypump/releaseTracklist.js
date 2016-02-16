14ed447ea326aa1aaf9acb0193b0255663b043b8
Emily Wilson <emily.wilson@teamspace.ca>
2016-02-16 15:07:57 -0400

    Added up until release 80

--- bodypump/releaseTracklist.js
+++ bodypump/releaseTracklist.js
@@ -154,8 +154,8 @@
 		},
 		"Release 92": {
 			"Warm Up": {
-				"Artist": "All We Need Is Love",
-				"Title":"Ricki-Lee"
+				"Artist": "Ricki-Lee",
+				"Title":"All We Need Is Love"
 			},
 			"Squats": {
 				"Artist": "Autoerotique vs Marc Noir",
@@ -188,6 +188,462 @@
 			"Abs": {
 				"Artist": "Iggy Azalea feat. Charli XCX",
 				"Title":"Fancy"
+			}
+		},
+		"Release 91": {
+			"Warm Up": {
+				"Artist": "Wrabel",
+				"Title":"Ten Feet Tall"
+			},
+			"Squats": {
+				"Artist": "The Killers",
+				"Title":"Somebody Told Me"
+			},
+			"Chest": {
+				"Artist": "Ellie Goulding",
+				"Title":"Goodness Gracious"
+			},
+			"Back": {
+				"Artist": "Armin Van Buuren",
+				"Title":"Beautiful Life (Remix)"
+			},
+			"Triceps": {
+				"Artist": "Avicii",
+				"Title":"Hey Brother"
+			},
+			"Biceps": {
+				"Artist": "Five Finger Death Punch",
+				"Title":"Bad Company"
+			},
+			"Lunges": {
+				"Artist": "Zedd",
+				"Title":"Find You"
+			},
+			"Shoulders": {
+				"Artist": "Fatboy Slim",
+				"Title":"Eat, Sleep, Rave, Repeat (Remix)"
+			},
+			"Abs": {
+				"Artist": "Jason Derulo",
+				"Title":"Trumpets"
+			}
+		},
+		"Release 90": {
+			"Warm Up": {
+				"Artist": "Avicii",
+				"Title":"You Make Me"
+			},
+			"Squats": {
+				"Artist": "Rihanna",
+				"Title":"What Now"
+			},
+			"Chest": {
+				"Artist": "Thirty Seconds to Mars",
+				"Title":"Conquistador"
+			},
+			"Back": {
+				"Artist": "Nero",
+				"Title":"Promises"
+			},
+			"Triceps": {
+				"Artist": "P!nk",
+				"Title":"Walk of Shame"
+			},
+			"Biceps": {
+				"Artist": "Panic! at the Disco feat. Loco",
+				"Title":"Miss Jackson"
+			},
+			"Lunges": {
+				"Artist": "Zedd feat. Hayley Williams",
+				"Title":"Stay the Night"
+			},
+			"Shoulders": {
+				"Artist": "Kill The Noise",
+				"Title":"Thumbs Up (For Rock N Roll)"
+			},
+			"Abs": {
+				"Artist": "OneRepublic",
+				"Title":"Something I need"
+			}
+		},
+		"Release 89": {
+			"Warm Up": {
+				"Artist": "Dane Rumble feat. Jupiter Project",
+				"Title":"Not Alone"
+			},
+			"Squats": {
+				"Artist": "R3Hab & Bassjackers",
+				"Title":"Raise Those Hands"
+			},
+			"Chest": {
+				"Artist": "Fall Out Boy",
+				"Title":"Where Did the Party Go"
+			},
+			"Back": {
+				"Artist": "Nero",
+				"Title":"Me & You"
+			},
+			"Triceps": {
+				"Artist": "Lady Gaga",
+				"Title":"Applause"
+			},
+			"Biceps": {
+				"Artist": "The Killers",
+				"Title":"When You Were Young"
+			},
+			"Lunges": {
+				"Artist": "Ellie Goulding",
+				"Title":"Burn"
+			},
+			"Shoulders": {
+				"Artist": "Brooklyn Bounce & DJ Zealot",
+				"Title":"True Hardstyler (Silver Nikan Radio Edit)"
+			},
+			"Abs": {
+				"Artist": "Naughty Boy feat. Sam Smith",
+				"Title":"La La La"
+			}
+		},
+		"Release 88": {
+			"Warm Up": {
+				"Artist": "Jessica Sanchez feat. Ne-Yo",
+				"Title":"Tonight"
+			},
+			"Squats": {
+				"Artist": "Skism feat. DC Breaks & Dee Freer",
+				"Title":"Killer (Crush Remix)"
+			},
+			"Chest": {
+				"Artist": "Fergie, Q-Tip & GoonRock",
+				"Title":"A Little Party Never Killed Nobody (All We Got)"
+			},
+			"Back": {
+				"Artist": "Jens O.",
+				"Title":"Somebody (Cc.K Remix)"
+			},
+			"Triceps": {
+				"Artist": "Macklemore & Ryan Lewis feat. Ray Dalton",
+				"Title":"Can't Hold Us"
+			},
+			"Biceps": {
+				"Artist": "Thirty Seconds to Mars",
+				"Title":"Up in the Air"
+			},
+			"Lunges": {
+				"Artist": "David Guetta feat. Ne-Yo & Akon",
+				"Title":"Play Hard"
+			},
+			"Shoulders": {
+				"Artist": "Brooklyn Bounce & Giorno",
+				"Title":"Raving (G! Mix)"
+			},
+			"Abs": {
+				"Artist": "Ellie Goulding",
+				"Title":"Only You"
+			}
+		},
+		"Release 87": {
+			"Warm Up": {
+				"Artist": "Christina Aguilera",
+				"Title":"Let There Be Love"
+			},
+			"Squats": {
+				"Artist": "Scooter",
+				"Title":"Black Betty"
+			},
+			"Chest": {
+				"Artist": "Pitbull ft. TJR",
+				"Title":"Don't Stop The Party"
+			},
+			"Back": {
+				"Artist": "Nick Skitz & Basslouder ft. Brooklyn Bounce",
+				"Title":"Again & Again (Club Mix)"
+			},
+			"Triceps": {
+				"Artist": "Fall Out Boy",
+				"Title":"My Songs Know What You Did In The Dark"
+			},
+			"Biceps": {
+				"Artist": "Sam And The Womp",
+				"Title":"Bom Bom"
+			},
+			"Lunges": {
+				"Artist": "Calvin Harris feat. Florence Welch",
+				"Title":"Sweet Nothing"
+			},
+			"Shoulders": {
+				"Artist": "Nero",
+				"Title":"Crush On You"
+			},
+			"Abs": {
+				"Artist": "Olly Murs ft. Flo Rida",
+				"Title":"Troublemaker"
+			}
+		},
+		"Release 86": {
+			"Warm Up": {
+				"Artist": "Flo Rida",
+				"Title":"I Cry"
+			},
+			"Squats": {
+				"Artist": "Six60",
+				"Title":"In The Clear"
+			},
+			"Chest": {
+				"Artist": "Christina Aquilera",
+				"Title":"Your Body"
+			},
+			"Back": {
+				"Artist": "DJ Flow",
+				"Title":"Campione 2012"
+			},
+			"Triceps": {
+				"Artist": "Train",
+				"Title":"50 Ways To Say Goodbye"
+			},
+			"Biceps": {
+				"Artist": "Kelly Clarkson",
+				"Title":"Dark Side"
+			},
+			"Lunges": {
+				"Artist": "Swedish House Mafia feat. John Martin",
+				"Title":"Don't You Worry Child (Radio Edit)"
+			},
+			"Shoulders": {
+				"Artist": "Upon Havoc",
+				"Title":"We'll Be Coming Back"
+			},
+			"Abs": {
+				"Artist": "Matchbox 20",
+				"Title":"She's So Mean"
+			}
+		},
+		"Release 85": {
+			"Warm Up": {
+				"Artist": "Alexandra Burke",
+				"Title":"Heartbreak On Hold"
+			},
+			"Squats": {
+				"Artist": "Swedish House Mafia vs. Knife Party",
+				"Title":"Antidote"
+			},
+			"Chest": {
+				"Artist": "Muse",
+				"Title":"Uprising"
+			},
+			"Back": {
+				"Artist": "Cascada",
+				"Title":"The Rhythm Of The Night (Ryan T. & Rick M. Radio Edit)"
+			},
+			"Triceps": {
+				"Artist": "Flo Rida",
+				"Title":"Let it Roll"
+			},
+			"Biceps": {
+				"Artist": "P!nk",
+				"Title":"Blow Me (One Last Kiss) (Clean Radio Edit)"
+			},
+			"Lunges": {
+				"Artist": "Loyal Crown",
+				"Title":"Spectrum"
+			},
+			"Shoulders": {
+				"Artist": "Brooklyn Bounce",
+				"Title":"This Is How We Rock! (Hardforze H.A.R.D Remix Edit)"
+			},
+			"Abs": {
+				"Artist": "Eastern Clause",
+				"Title":"One More Night"
+			}
+		},
+		"Release 84": {
+			"Warm Up": {
+				"Artist": "Lock Jaw",
+				"Title":"Let's Go"
+			},
+			"Squats": {
+				"Artist": "Element Depot",
+				"Title":"Turning Tables"
+			},
+			"Chest": {
+				"Artist": "Guiltless United",
+				"Title":"Welcome To The Jungle"
+			},
+			"Back": {
+				"Artist": "Cascada",
+				"Title":"Because the Night (Manian Bootleg Cut)"
+			},
+			"Triceps": {
+				"Artist": "Usher",
+				"Title":"Scream"
+			},
+			"Biceps": {
+				"Artist": "Kayplex",
+				"Title":"Crazy"
+			},
+			"Lunges": {
+				"Artist": "Reality Hero",
+				"Title":"Where Have You Been"
+			},
+			"Shoulders": {
+				"Artist": "Manian",
+				"Title":"Welcome To The Club 2011 (Discotronic Radio Edit)"
+			},
+			"Abs": {
+				"Artist": "Flo Rida",
+				"Title":"Whistle"
+			}
+		},
+		"Release 83": {
+			"Warm Up": {
+				"Artist": "Pimp Dawg",
+				"Title":"Set It Off (Wings & Rider Remix Edit)"
+			},
+			"Squats": {
+				"Artist": "Yesterday Affair",
+				"Title":"Summer Of '69"
+			},
+			"Chest": {
+				"Artist": "Chris Brown",
+				"Title":"Turn Up The Music"
+			},
+			"Back": {
+				"Artist": "Fizzy Deejay",
+				"Title":"Set Fire To The Rain (B&W Remix)"
+			},
+			"Triceps": {
+				"Artist": "Toy Hypo",
+				"Title":"Glad You Came"
+			},
+			"Biceps": {
+				"Artist": "Kelly Clarkson",
+				"Title":"Stronger (What Doesn't Kill You)"
+			},
+			"Lunges": {
+				"Artist": "Manian & Aila",
+				"Title":"Turn The Tide (Manox Remix)"
+			},
+			"Shoulders": {
+				"Artist": "Scooter feat. Vicky Leandros",
+				"Title":"C'est Bleu"
+			},
+			"Abs": {
+				"Artist": "Nickelback",
+				"Title":"When We Stand Together"
+			}
+		},
+		"Release 82": {
+			"Warm Up": {
+				"Artist": "Ricki-Lee",
+				"Title":"Raining Diamonds"
+			},
+			"Squats": {
+				"Artist": "N-Finity",
+				"Title":"Someone Like You (Kris McTwain Club Edit)"
+			},
+			"Chest": {
+				"Artist": "Solitary Phase",
+				"Title":"I Like How It Feels"
+			},
+			"Back": {
+				"Artist": "Jan Wayne",
+				"Title":"Bring Me To Life (Hands Up Club Mix)"
+			},
+			"Triceps": {
+				"Artist": "Secret Appendix",
+				"Title":"Bad Case Of Loving You"
+			},
+			"Biceps": {
+				"Artist": "Nickelback",
+				"Title":"Bottoms Up"
+			},
+			"Lunges": {
+				"Artist": "Sash!",
+				"Title":"Ecuador (Bad Behaviour Remix)"
+			},
+			"Shoulders": {
+				"Artist": "Paffendorf",
+				"Title":"Be Cool 2011 (DJ From Mars Club Remix)"
+			},
+			"Abs": {
+				"Artist": "Calvin Harris",
+				"Title":"Feel So Close"
+			}
+		},
+		"Release 81": {
+			"Warm Up": {
+				"Artist": "Cobra Starship feat. Sabi",
+				"Title":"You Make Me Feel…"
+			},
+			"Squats": {
+				"Artist": "Jagged Eagle",
+				"Title":"Higher Ground"
+			},
+			"Chest": {
+				"Artist": "Stan Walker",
+				"Title":"Loud"
+			},
+			"Back": {
+				"Artist": "Manian",
+				"Title":"Ravers Fantasy (Manox Remix)"
+			},
+			"Triceps": {
+				"Artist": "Miss Daise",
+				"Title":"Domino"
+			},
+			"Biceps": {
+				"Artist": "Shinedown",
+				"Title":"I Own You"
+			},
+			"Lunges": {
+				"Artist": "Fine Tune",
+				"Title":"San Francisco (Remix II)"
+			},
+			"Shoulders": {
+				"Artist": "Blade X",
+				"Title":"Blood Is Pumping"
+			},
+			"Abs": {
+				"Artist": "Alexandra Stan",
+				"Title":"Mr Saxobeat"
+			}
+		},
+		"Release 80": {
+			"Warm Up": {
+				"Artist": "September",
+				"Title":"Resuscitate Me"
+			},
+			"Squats": {
+				"Artist": "Red Beans & Rice",
+				"Title":"The Edge Of Glory (Glory Mix)"
+			},
+			"Chest": {
+				"Artist": "Ninth Immortal",
+				"Title":"Smells Like Teen Spirit"
+			},
+			"Back": {
+				"Artist": "Twilight",
+				"Title":"The Tide"
+			},
+			"Triceps": {
+				"Artist": "Britney Spears",
+				"Title":"Till The World Ends"
+			},
+			"Biceps": {
+				"Artist": "Altiyan Childs",
+				"Title":"Eye Of The Tiger"
+			},
+			"Lunges": {
+				"Artist": "Dan Winter",
+				"Title":"Fading Like A Flower (Club Radio Mix)"
+			},
+			"Shoulders": {
+				"Artist": "Jens O.",
+				"Title":"Hands Up! (Sample Ripperz Remix)"
+			},
+			"Abs": {
+				"Artist": "Colourbox",
+				"Title":"Rolling In The Deep"
 			}
 		}
 	}
